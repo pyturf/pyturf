568e807daf1eeb0e50636fa951c985f97f4d1bc7
diogomatoschaves <diogo_chaves@hotmail.com>
2020-03-21 13:50:19 +0100

    Add tests for GeoJSON features

--- turf/helpers/tests/test_features.py
+++ turf/helpers/tests/test_features.py
@@ -1,15 +1,26 @@
 import pytest
 
-from turf.helpers._features import point, line_string
+from turf.helpers._features import (
+    point,
+    line_string,
+    polygon,
+    points,
+    line_strings,
+    polygons,
+    multi_point,
+    multi_line_string,
+    multi_polygon,
+)
 
 
 class TestPoint:
     def test_coordinates_props(self):
 
-        p = point([5, 10], {"name": 'test point'})
+        p = point([5, 10], {"name": "test point"})
 
         assert p.geometry.coordinates[0] == 5
         assert p.geometry.coordinates[1] == 10
+        assert p.geometry.type == "Point"
         assert p.properties["name"] == "test point"
 
     def test_no_props(self):
@@ -18,63 +29,188 @@ class TestPoint:
 
         assert p.properties == {}
 
+    def test_exceptions(self):
+
+        with pytest.raises(Exception):
+            point()
+
+        with pytest.raises(Exception) as excinfo:
+            point([1])
+        assert "[lng, lat]" in str(excinfo.value)
+
+        with pytest.raises(Exception) as excinfo:
+            point([1, "xyz"])
+        assert "invalid" in str(excinfo.value)
+
+    def test_points(self):
+
+        pts = points([[-75, 39], [-80, 45], [-78, 50]], {"foo": "bar"}, {"id": "hello"})
+
+        assert len(pts.features) == 3
+        assert pts.id == "hello"
+        assert pts.features[0].properties["foo"] == "bar"
+
+    def test_multi_point(self):
+
+        mp = multi_point([[0, 0], [10, 10]], {"test": 23})
+
+        assert mp.type == "Feature"
+        assert mp.properties == {"test": 23}
+        assert mp.geometry.type == "MultiPoint"
+        assert mp.geometry.coordinates == [[0, 0], [10, 10]]
+
 
 class TestLineString:
-
     def test_coordinates_props(self):
 
-        line = line_string([[5, 10], [20, 40]], {"name": 'test line'});
+        line = line_string([[5, 10], [20, 40]], {"name": "test line"})
 
         assert line.geometry.coordinates[0][0] == 5
         assert line.geometry.coordinates[1][0] == 20
+        assert line.geometry.type == "LineString"
         assert line.properties["name"] == "test line"
 
-# test('lineString', t => {
-#     const line = lineString([[5, 10], [20, 40]], {name: 'test line'});
-# t.ok(line, 'creates a linestring');
-# t.equal(line.geometry.coordinates[0][0], 5);
-# t.equal(line.geometry.coordinates[1][0], 20);
-# t.equal(line.properties.name, 'test line');
-# t.deepEqual(lineString([[5, 10], [20, 40]]).properties, {}, 'no properties case');
-#
-# t.throws(() => lineString(), 'error on no coordinates');
-# t.throws(() => lineString([[5, 10]]), 'coordinates must be an array of two or more positions');
-# t.throws(() => lineString([['xyz', 10]]), 'coordinates must contain numbers');
-# t.throws(() => lineString([[5, 'xyz']]), 'coordinates must contain numbers');
-# t.end();
-# });
+    def test_no_props(self):
+
+        line = line_string([[5, 10], [20, 40]])
+
+        assert line.properties == {}
+
+    def test_exceptions(self):
+
+        with pytest.raises(Exception):
+            line_string()
+
+        with pytest.raises(Exception) as excinfo:
+            line_string([[5, 10]])
+        assert "two or more" in str(excinfo.value)
+
+        with pytest.raises(Exception) as excinfo:
+            line_string([["xyz", 10], [1, 4]])
+        assert "invalid" in str(excinfo.value)
+
+    def test_line_strings(self):
+
+        ls = line_strings(
+            [
+                [[-24, 63], [-23, 60], [-25, 65], [-20, 69]],
+                [[-14, 43], [-13, 40], [-15, 45], [-10, 49]],
+            ],
+            {"foo": "bar"},
+            {"id": "hello"},
+        )
+
+        assert len(ls.features) == 2
+        assert ls.id == "hello"
+        assert ls.features[0].properties["foo"] == "bar"
+
+    def test_multi_line_string(self):
+
+        mls = multi_line_string([[[0, 0], [1, 2]], [[5, 0], [15, 8]]], {"test": 23})
+
+        assert mls.type == "Feature"
+        assert mls.properties == {"test": 23}
+        assert mls.geometry.type == "MultiLineString"
+        assert mls.geometry.coordinates == [[[0, 0], [1, 2]], [[5, 0], [15, 8]]]
+
+    def test_multi_line_string_exception(self):
+
+        with pytest.raises(Exception) as excinfo:
+            multi_line_string([[[0, 0]], [[5, 0], [15, 8]]], {"test": 23})
+        assert "two or more" in str(excinfo.value)
 
 
-# test('polygon', t => {
-#     const poly = polygon([[[5, 10], [20, 40], [40, 0], [5, 10]]], {name: 'test polygon'});
-# t.ok(poly);
-# t.equal(poly.geometry.coordinates[0][0][0], 5);
-# t.equal(poly.geometry.coordinates[0][1][0], 20);
-# t.equal(poly.geometry.coordinates[0][2][0], 40);
-# t.equal(poly.properties.name, 'test polygon');
-# t.equal(poly.geometry.type, 'Polygon');
-# t.throws(() => {
-#     t.equal(polygon([[[20.0, 0.0], [101.0, 0.0], [101.0, 1.0], [100.0, 1.0], [100.0, 0.0]]]).message);
-# }, /First and last Position are not equivalent/, 'invalid ring - not wrapped');
-# t.throws(() => {
-#     t.equal(polygon([[[20.0, 0.0], [101.0, 0.0]]]).message);
-# }, /Each LinearRing of a Polygon must have 4 or more Positions/, 'invalid ring - too few positions');
-# const noProperties = polygon([[[5, 10], [20, 40], [40, 0], [5, 10]]]);
-# t.deepEqual(noProperties.properties, {});
-# t.end();
-# });
-#
-# test('lineString', t => {
-#     const line = lineString([[5, 10], [20, 40]], {name: 'test line'});
-# t.ok(line, 'creates a linestring');
-# t.equal(line.geometry.coordinates[0][0], 5);
-# t.equal(line.geometry.coordinates[1][0], 20);
-# t.equal(line.properties.name, 'test line');
-# t.deepEqual(lineString([[5, 10], [20, 40]]).properties, {}, 'no properties case');
-#
-# t.throws(() => lineString(), 'error on no coordinates');
-# t.throws(() => lineString([[5, 10]]), 'coordinates must be an array of two or more positions');
-# t.throws(() => lineString([['xyz', 10]]), 'coordinates must contain numbers');
-# t.throws(() => lineString([[5, 'xyz']]), 'coordinates must contain numbers');
-# t.end();
-# });
+class TestPolygon:
+    def test_coordinates_props(self):
+
+        poly = polygon(
+            [[[5, 10], [20, 40], [40, 0], [5, 10]]], {"name": "test polygon"}
+        )
+
+        assert poly.geometry.coordinates[0][0][0] == 5
+        assert poly.geometry.coordinates[0][1][0] == 20
+        assert poly.geometry.coordinates[0][2][0] == 40
+        assert poly.properties["name"] == "test polygon"
+        assert poly.geometry.type == "Polygon"
+
+    def test_no_props(self):
+
+        poly = polygon([[[5, 10], [20, 40], [40, 0], [5, 10]]])
+
+        assert poly.properties == {}
+
+    def test_exceptions(self):
+
+        with pytest.raises(Exception):
+            polygon()
+
+        with pytest.raises(Exception) as excinfo:
+            polygon("xyz")
+        assert "list of LinearRing" in str(excinfo.value)
+
+        with pytest.raises(Exception) as excinfo:
+            polygon(["xyz"])
+        assert "list of Positions" in str(excinfo.value)
+
+        with pytest.raises(Exception) as excinfo:
+            polygon([[[5, 10], [20, 40], [40, 0]]])
+        assert "4 or more Positions" in str(excinfo.value)
+
+        with pytest.raises(Exception) as excinfo:
+            polygon([[[5, 10], [20, 40], [40, 0], [2, 3]]])
+        assert "not equivalent" in str(excinfo.value)
+
+    def test_polygons(self):
+
+        poly = polygons(
+            [
+                [[[-5, 52], [-4, 56], [-2, 51], [-7, 54], [-5, 52]]],
+                [[[-15, 42], [-14, 46], [-12, 41], [-17, 44], [-15, 42]]],
+            ],
+            {"foo": "bar"},
+            {"id": "hello"},
+        )
+
+        assert len(poly.features) == 2
+        assert poly.id == "hello"
+        assert poly.features[0].properties["foo"] == "bar"
+
+    def test_multi_polygon(self):
+
+        mp = multi_polygon(
+            [
+                [[[94, 57], [78, 49], [94, 43], [94, 57]]],
+                [[[93, 19], [63, 7], [79, 0], [93, 19]]],
+            ],
+            {"test": 23},
+        )
+
+        assert mp.type == "Feature"
+        assert mp.properties == {"test": 23}
+        assert mp.geometry.type == "MultiPolygon"
+        assert mp.geometry.coordinates == [
+            [[[94, 57], [78, 49], [94, 43], [94, 57]]],
+            [[[93, 19], [63, 7], [79, 0], [93, 19]]],
+        ]
+
+    def test_multi_polygon_exception(self):
+
+        with pytest.raises(Exception) as excinfo:
+            multi_polygon(
+                [
+                    [[[0, 1], [78, 49], [94, 43], [94, 57]]],
+                    [[[93, 19], [63, 7], [79, 0], [93, 19]]],
+                ],
+                {"test": 23},
+            )
+        assert "not equivalent" in str(excinfo.value)
+
+        with pytest.raises(Exception) as excinfo:
+            multi_polygon(
+                [
+                    [[[78, 49], [94, 43], [94, 57]]],
+                    [[[93, 19], [63, 7], [79, 0], [93, 19]]],
+                ],
+                {"test": 23},
+            )
+        assert "4 or more Positions" in str(excinfo.value)
