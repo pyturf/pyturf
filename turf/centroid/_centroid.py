75950033e0bf725b1fb7053bb5997e8f964d0633
diogomatoschaves <diogo_chaves@hotmail.com>
2020-04-06 23:54:55 +0100

    Add centroid module

---
+++ turf/centroid/_centroid.py
@@ -0,0 +1,40 @@
+from functools import reduce
+
+from turf.helpers import point
+from turf.invariant import get_coords_from_features
+from turf.utils.helpers import get_input_dimensions
+
+
+def centroid(features, options=None):
+    """
+    Takes one or more features and calculates the centroid using the mean of all vertices.
+    This lessens the effect of small islands and artifacts when calculating the centroid of a set of polygons.
+
+    :param features: GeoJSON features to be centered
+    :param options: optional parameters
+        [options["properties"]={}] Translate GeoJSON Properties to Point
+    :return: a Point feature corresponding to the centroid of the input features
+    """
+
+    if not options:
+        options = {}
+
+    coords = get_coords_from_features(features)
+
+    if get_input_dimensions(coords) == 1:
+        coords = [coords]
+
+    x_sum, y_sum, length = reduce(reduce_coords, coords, [0, 0, 0])
+
+    return point([x_sum / length, y_sum / length], options.get('properties', None)).to_geojson()
+
+
+def reduce_coords(sum_array, coords):
+
+    input_dimension = get_input_dimensions(coords)
+
+    if input_dimension >= 2:
+        return reduce(lambda prev, coord: reduce_coords(prev, coord), coords, sum_array)
+
+    return [sum_array[0] + coords[0], sum_array[1] + coords[1], sum_array[2] + 1]
+
---
+++ turf/centroid/tests/test_centroid.py
@@ -0,0 +1,64 @@
+import pytest
+import os
+
+from turf.centroid import centroid
+
+from turf.utils.error_codes import error_code_messages
+from turf.utils.exceptions import InvalidInput
+from turf.utils.test_setup import get_fixtures
+
+current_path = os.path.dirname(os.path.realpath(__file__))
+
+fixtures = get_fixtures(current_path)
+
+
+class TestCentroid:
+
+    allowed_types = [
+        "Point",
+        "MultiPoint",
+        "LineString",
+        "MultiLineString",
+        "Polygon",
+        "MultiPolygon",
+    ]
+
+    @pytest.mark.parametrize(
+        "fixture",
+        [
+            pytest.param(fixture, id=fixture_name)
+            for fixture_name, fixture in fixtures.items()
+        ],
+    )
+    def test_centroid(self, fixture):
+
+        options = {
+            "properties": {
+                "marker-symbol": "circle"
+            }
+        }
+
+        assert centroid(fixture["in"], options) == fixture["out"]
+
+    @pytest.mark.parametrize(
+        "input_value, exception_value",
+        [
+            pytest.param(
+                "xyz",
+                error_code_messages["InvalidGeometry"](allowed_types),
+                id="InvalidGeometry",
+            ),
+            pytest.param(
+                [1],
+                error_code_messages["InvalidPointInput"],
+                id="InvalidPointInput",
+            ),
+        ],
+    )
+    def test_exception(self, input_value, exception_value):
+
+        with pytest.raises(Exception) as excinfo:
+            centroid(input_value)
+
+        assert excinfo.type == InvalidInput
+        assert str(excinfo.value) == exception_value
