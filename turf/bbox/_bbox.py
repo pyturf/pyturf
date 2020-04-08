159593372cce2b68947fedfd3f5e6468157807b7
diogomatoschaves <diogo_chaves@hotmail.com>
2020-04-08 16:02:53 +0100

    Fix bug in bbox method

--- turf/bbox/_bbox.py
+++ turf/bbox/_bbox.py
@@ -6,7 +6,7 @@ from turf.invariant import get_coords_from_features
 from turf.utils.helpers import get_input_dimensions
 
 
-def bbox(features, *args):
+def bbox(features):
     """
     Takes a set of features and returns a bounding box containing of all input features.
 
@@ -14,28 +14,30 @@ def bbox(features, *args):
     :return: bounding box extent in [minX, minY, maxX, maxY] order
     """
 
-    if args:
-        bounding_box = args[0]
-    else:
-        bounding_box = [np.inf, np.inf, -np.inf, -np.inf]
+    bounding_box = [np.inf, np.inf, -np.inf, -np.inf]
 
     coords = get_coords_from_features(features)
 
-    input_dimension = get_input_dimensions(coords)
-
-    if input_dimension > 2:
-        return reduce(lambda bb, coord: bbox(coord, bb), coords, bounding_box)
-    elif input_dimension == 1:
+    if get_input_dimensions(coords) == 1:
         coords = [coords]
 
-    for coord in coords:
-        if bounding_box[0] > coord[0]:
-            bounding_box[0] = coord[0]
-        if bounding_box[1] > coord[1]:
-            bounding_box[1] = coord[1]
-        if bounding_box[2] < coord[0]:
-            bounding_box[2] = coord[0]
-        if bounding_box[3] < coord[1]:
-            bounding_box[3] = coord[1]
+    return reduce(reduce_coords, coords, bounding_box)
+
+
+def reduce_coords(bounding_box, coord):
+
+    input_dimension = get_input_dimensions(coord)
+
+    if input_dimension >= 2:
+        return reduce(lambda bb, c: reduce_coords(bb, c), coord, bounding_box)
+
+    if bounding_box[0] > coord[0]:
+        bounding_box[0] = coord[0]
+    if bounding_box[1] > coord[1]:
+        bounding_box[1] = coord[1]
+    if bounding_box[2] < coord[0]:
+        bounding_box[2] = coord[0]
+    if bounding_box[3] < coord[1]:
+        bounding_box[3] = coord[1]
 
     return bounding_box
