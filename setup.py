3cf71f436bba00aa17d8fb8858af89441d9eaf85
diogomatoschaves <diogo_chaves@hotmail.com>
2020-04-13 20:13:10 +0100

    Add Pypi publishing CI pipeline

--- setup.py
+++ setup.py
@@ -1,11 +1,20 @@
+import os
 from setuptools import setup, find_packages
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+try:
+    version = os.environ["GITHUB_REF"].split("-")[-1]
+    print(f"Version: {version}")
+except KeyError:
+    with open("version.txt") as f:
+        __version__ = f.read()
+    version = __version__
+
 setup(
     name="pyturf",
-    version="0.1",
+    version=version,
     description="Python geospatial library",
     long_description=long_description,
     long_description_content_type="text/markdown",
