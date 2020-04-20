52fb12e6a34dfaaa38bba815e3a6a421296f9cd3
diogomatoschaves <diogo_chaves@hotmail.com>
2020-04-20 18:49:39 +0100

    Get version number from python file

--- scripts/update_version.py
+++ scripts/update_version.py
@@ -3,7 +3,4 @@ import os
 version = os.environ["GITHUB_REF"].split("/")[-1]
 
 with open("turf/version.py", "w") as f:
-    f.write(
-        f"""\
-__version__ = '{str(version)}'\n"""
-    )
+    f.write(f"""__version__ = '{str(version)}'\n""")
